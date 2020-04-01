6bbfb41ccab2b7a7a17d82b182c46e2f895e9892
Leon Hannah Tabak <l.tabak@ieee.org>
2020-04-01 08:49:58 -0500

    feat: Rewrite class to model a 4 x 4 rather than a 2 x 2 matrix.
    Create a class that can be used in experiments with 3-D geometry.

--- src/main/java/com/eonsahead/matrix/Matrix.java
+++ src/main/java/com/eonsahead/matrix/Matrix.java
@@ -2,69 +2,90 @@ package com.eonsahead.matrix;
 
 /**
  * Model a matrix.
- * 
+ *
  * @author Leon Tabak
  * @version 1 April 2020
  */
 public class Matrix {
 
-    private double[][] elements = new double[2][2];
+    private double[][] elements;
 
     public Matrix() {
-        this.elements[0][0] = 1.0;
-        this.elements[0][1] = 0.0;
-        this.elements[1][0] = 0.0;
-        this.elements[1][1] = 1.0;
+        double[] row0 = {1, 0, 0, 0};
+        double[] row1 = {0, 1, 0, 0};
+        double[] row2 = {0, 0, 1, 0};
+        double[] row3 = {0, 0, 0, 1};
+        double[][] e = {row0, row1, row2, row3};
+        this.elements = e;
     } // Matrix()
 
-    public Matrix(double m00, double m01, double m10, double m11) {
-        this.elements[0][0] = m00;
-        this.elements[0][1] = m01;
-        this.elements[1][0] = m10;
-        this.elements[1][1] = m11;
-    } // Matrix( double, double, double, double )
-
-    public double get( int row, int column ) {
+    public double get(int row, int column) {
         return this.elements[row][column];
     } // get( int, int )
+
+    public void set(int row, int column, double value) {
+        this.elements[row][column] = value;
+    } // set( int, int, double )
+
+    public void identity() {
+        for( int i = 0; i < 4; i++ ) {
+            for( int j = 0; j < 4; j++ ) {
+                if( i == j ) {
+                    this.elements[i][j] = 1.0;
+                } // if
+                else {
+                    this.elements[i][j] = 0.0;
+                } // else
+            } // for
+        } // for
+    } // identity()
     
-    public void rotation(double angle) {
-        this.elements[0][0] = Math.cos( angle );
-        this.elements[0][1] = -Math.sin(angle);
-        this.elements[1][0] = Math.sin(angle);
-        this.elements[1][1] = Math.cos(angle);
-    } // rotation( double )
+    public void rotationZ(double angle) {
+        this.identity();
+        this.set( 0, 0, Math.cos(angle) );
+        this.set( 0, 1, -Math.sin(angle) );
+        this.set( 1, 0, Math.sin(angle) );
+        this.set( 1, 1, Math.cos(angle) );
+    } // rotationZ( double )
 
     public Matrix multiply(Matrix otherMatrix) {
-        double left00 = this.elements[0][0];
-        double left01 = this.elements[0][1];
-        double left10 = this.elements[1][0];
-        double left11 = this.elements[1][1];
-
-        double right00 = otherMatrix.elements[0][0];
-        double right01 = otherMatrix.elements[0][1];
-        double right10 = otherMatrix.elements[1][0];
-        double right11 = otherMatrix.elements[1][1];
-
-        double product00 = left00 * right00 + left01 * right10;
-        double product01 = left00 * right01 + left01 * right11;
-        double product10 = left10 * right00 + left11 * right10;
-        double product11 = left10 * right01 + left11 * right11;
-
-        return new Matrix(product00, product01, product10, product11);
+        Matrix product = new Matrix();
+        for( int row = 0; row < 4; row++ ) {
+            for( int column = 0; column < 4; column++ ) {
+                double sum = 0.0;
+                for( int k = 0; k < 4; k++ ) {
+                    sum += this.elements[row][k] * 
+                            otherMatrix.elements[k][column];
+                } // for
+                product.set( row, column, sum );
+            } // for
+        } // for
+        return product;
     } // multiply( Matrix )
+
+    private String rowToString(int row) {
+        StringBuilder result = new StringBuilder();
+        result.append("( ");
+        result.append(this.elements[row][0]);
+        result.append(",");
+        result.append(this.elements[row][1]);
+        result.append(",");
+        result.append(this.elements[row][2]);
+        result.append(",");
+        result.append(this.elements[row][3]);
+        result.append(" )");
+        return result.toString();
+    } // rowToString( int )
 
     @Override
     public String toString() {
-        double m00 = this.elements[0][0];
-        double m01 = this.elements[0][1];
-        double m10 = this.elements[1][0];
-        double m11 = this.elements[1][1];
-
-        String row0 = "(" + m00 + "," + m01 + ")";
-        String row1 = "(" + m10 + "," + m11 + ")";
-
-        return "[" + row0 + "," + row1 + "]";
+        StringBuilder result = new StringBuilder();
+        result.append( "[ " );
+        for( int i = 0; i < 4; i++ ) {
+            result.append( rowToString(i) );
+        } //
+        result.append( " ]" );
+        return result.toString();
     } // toString()
 
     public static void main(String[] args) {
@@ -73,17 +94,17 @@ public class Matrix {
 
         Matrix product = identity.multiply(identity);
         System.out.println("product = " + product);
-        
+
         Matrix ccw = new Matrix();
-        ccw.rotation( Math.PI/4 );
-        System.out.println( "counter-clockwise rotation = " + ccw );
-        
+        ccw.rotationZ(Math.PI / 4);
+        System.out.println("counter-clockwise rotation = " + ccw);
+
         Matrix cw = new Matrix();
-        cw.rotation( -Math.PI/4 );
-        System.out.println( "clockwise rotation = " + cw );
-        
-        Matrix netRotation = ccw.multiply( cw );
-        System.out.println( "net rotation = " + netRotation );
+        cw.rotationZ(-Math.PI / 4);
+        System.out.println("clockwise rotation = " + cw);
+
+        Matrix netRotation = ccw.multiply(cw);
+        System.out.println("net rotation = " + netRotation);
     } // main( String [] )
 
 } // Matrix
